eaee4f6a571af830b0fd42914ade36d35cc09d63
pedromst2000 <40210465@esmad.ipp.pt>
2023-09-02 16:54:45 +0100

    inserted icon for the app

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import * 
+
+Window = Tk()
+Window.title("CraftingCook")
+Window.geometry("500x500")
+
+# to insert the icon on the window
+Window.iconbitmap("assets/CraftingCook.ico")
+
+
+
+
+Window.mainloop()
