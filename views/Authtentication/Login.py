3f74b1c5ab42a26d1c61a6782963397a5d6e32f2
pedromst2000 <40210465@esmad.ipp.pt>
2023-09-15 19:15:53 +0100

    updated

--- views/Authtentication/Login.py
+++ views/Authtentication/Login.py
@@ -5,5 +5,22 @@ from models.Authenticate import *
 
 
 def loginView():
-    print("loginView")
+    # open the window
+    loginWindow = tk.Toplevel()
+    loginWindow.title("Login")
+    loginWindow.iconbitmap("assets/CraftingCook.ico")
+    loginWindow.geometry("580x534")
+    loginWindow.resizable(0, 0)
+    loginWindow.config(bg="#806B14")
 
+    # canvas
+    canvasLogin = Canvas(loginWindow, bg="#806B14", height=534, width=580)
+    canvasLogin.place(x=0, y=0)
+
+    canvasLogin.image = tk.PhotoImage(file="assets/images/login_image.png")
+
+    canvasLogin.create_image(0, 0, anchor=NW, image=canvasLogin.image)
+
+    
+
+    loginWindow.grab_set()
